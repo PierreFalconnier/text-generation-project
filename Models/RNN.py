87083ab5752fc3ee22fb41110902d2b8169ed713
Falconnier <pierrefalco@hotmail.fr>
2024-05-04 01:23:45 +0200

    added prediction method to generate text

--- Models/RNN.py
+++ Models/RNN.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
 
 
 class RNN(nn.Module):
@@ -48,3 +50,68 @@ class RNN(nn.Module):
 
     def init_state(self, batch_size):
         return torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+
+    def generate(self, dataset, device, text, total_length=100):
+        self.eval()
+
+        words = text.split(" ")
+        state_h = self.init_state(1).to(device)
+
+        for i in range(0, total_length):
+            x = torch.tensor([[dataset.word_to_index[w] for w in words[i:]]]).to(device)
+
+            if self.embedding_dim is None:
+                x = F.one_hot(x, num_classes=self.vocab_size).float()
+
+            y_pred, state_h = self(x, state_h)  # B,  sequence_length, vocabsize
+
+            last_word_logits = y_pred[0][-1]
+            p = (
+                torch.nn.functional.softmax(last_word_logits, dim=0)
+                .detach()
+                .cpu()
+                .numpy()
+            )
+            word_index = np.random.choice(len(last_word_logits), p=p)
+            words.append(dataset.index_to_word[word_index])
+
+        return words
+
+
+if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    CUR_DIR_PATH = Path(__file__)
+    ROOT = CUR_DIR_PATH.parents[1]
+    if str(ROOT) not in sys.path:
+        sys.path.append(str(ROOT))
+    from Dataset.DatasetShakespeare import DatasetShakespeare as Dataset
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    # DATASET
+    folder_path = ROOT / "Data" / "shakespeare"
+    dataset = Dataset(
+        folder_path=folder_path,
+        sequence_length=5,
+    )
+
+    #   TRAIN
+    model = RNN(
+        vocab_size=dataset.vocab_size,
+        hidden_dim=100,
+        embedding_dim=None,
+        num_layers=1,
+        dropout=0.0,
+        nonlinearity="tanh",
+    ).to(device)
+
+    list_text = model.generate(
+        dataset,
+        device=device,
+        text="This is a test to make sure that",
+        total_length=100,
+    )
+    print(" ".join(list_text))
