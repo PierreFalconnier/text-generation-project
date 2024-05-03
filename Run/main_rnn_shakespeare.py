ea3f517794d7d941c452299bcc2af83661c5d2cf
Falconnier <pierrefalco@hotmail.fr>
2024-05-04 01:24:27 +0200

    added logger and parser. ready to train

--- Run/main_rnn_shakespeare.py
+++ Run/main_rnn_shakespeare.py
@@ -1,8 +1,25 @@
 from pathlib import Path
 import torch
 import sys
+import argparse
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import torch.nn.functional as F
+from tqdm import tqdm
 
 if __name__ == "__main__":
+
+    # Hyperparams
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--sequence-length", type=int, default=5)
+    parser.add_argument("--embedding-dim", type=int, default=None)
+    parser.add_argument("--hidden-dim", type=int, default=100)
+    parser.add_argument("--num-layers", type=int, default=1)
+    parser.add_argument("--dropout", type=float, default=0.0)
+    args = parser.parse_args()
 
     # IMPORTATIONS
     # include the path of the dataset(s) and the model(s)
@@ -13,40 +30,49 @@ if __name__ == "__main__":
 
     from Dataset.DatasetShakespeare import DatasetShakespeare as Dataset
     from Models.RNN import RNN
-    from torch.utils.data import DataLoader
-    from torch import nn, optim
-    import torch.nn.functional as F
-    from tqdm import tqdm
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     # DATASET
     folder_path = ROOT / "Data" / "shakespeare"
-    sequence_length = 25
     dataset = Dataset(
         folder_path=folder_path,
-        sequence_length=sequence_length,
+        sequence_length=args.sequence_length,
     )
 
     # DATALOADER
-    batch_size = 32
-    dataloader = DataLoader(dataset, batch_size=batch_size)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size)
+
+    # LOGGER
+    name = Path(__file__).name[:-3]
+    name += (
+        "_"
+        + str(args.epochs)
+        + "_"
+        + str(args.batch_size)
+        + "_"
+        + str(args.sequence_length)
+    )
+    LOG_DIR = ROOT / "Run" / "Results" / name
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+    writer = SummaryWriter(log_dir=LOG_DIR)
 
     #   TRAIN
     model = RNN(
         vocab_size=dataset.vocab_size,
-        hidden_dim=100,
-        embedding_dim=None,
-        num_layers=1,
-        dropout=0.0,
+        hidden_dim=args.hidden_dim,
+        embedding_dim=args.embedding_dim,
+        num_layers=args.num_layers,
+        dropout=args.dropout,
         nonlinearity="tanh",
     ).to(device)
     criterion = nn.CrossEntropyLoss()  # include softmax !
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    epochs = 1
 
-    for epoch in tqdm(range(epochs)):
+    for epoch in tqdm(range(args.epochs)):
+        model.train()
+        train_loss = 0.0
         state_h = None
 
         for x, y in tqdm(dataloader):
@@ -62,10 +88,24 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             y_pred, state_h = model(x, state_h)
 
-            # y_pred to batch_size, num_classes, sequence_length
+            # y_pred shape = B,  sequence_length, vocabsize
+            # y_pred to B, vocabsize, sequence_length
             loss = criterion(y_pred.permute(0, 2, 1), y)
             loss.backward()
             optimizer.step()
+            train_loss += loss.item()
 
             # avoid backprop across batchs
             state_h = state_h.detach()
+
+        # Validation, generation, logger
+        model.eval()
+        list_text = model.generate(
+            dataset,
+            device=device,
+            text="What is your name?",
+            total_length=50,
+        )
+        text = " ".join(list_text)
+        writer.add_scalars("loss", {"train": train_loss}, epoch)
+        writer.add_text("text_generation", text, epoch)
