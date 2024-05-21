33901c9c01daaae0c2bc87ac77ef6b4daa8bbf27
Falconnier <pierrefalco@hotmail.fr>
2024-05-21 18:06:57 +0200

    corrected the nucleous sampling to take into account that the highest proba is greater than top_p and corrected to take the right word index

--- Run/run.sh
+++ Run/run.sh
@@ -59,8 +59,15 @@
 # python /home/pierre/Documents/kth/Deep_Learning_in_data_science_DD2424/text-generation-project/Run/add_missing_metrics_lstm.py --hidden-dim 2048 --num-layers 2
 
 
-# GRID SEARCH
-python /home/pierre/Documents/kth/Deep_Learning_in_data_science_DD2424/text-generation-project/Run/main_train_lstm_gridsearch2.py --hidden-dim 1024 --num-layers 1
-/bin/python3.10 /home/pierre/Documents/kth/Speech_and_Speaker_recognition_DT2119/speech-denoising-project/train_unet.py
+# # GRID SEARCH
+# python /home/pierre/Documents/kth/Deep_Learning_in_data_science_DD2424/text-generation-project/Run/main_train_lstm_gridsearch2.py --hidden-dim 1024 --num-layers 1
+# /bin/python3.10 /home/pierre/Documents/kth/Speech_and_Speaker_recognition_DT2119/speech-denoising-project/train_unet.py
 
-shutdown -h
+# shutdown -h
+
+# /bin/python3.10 /home/pierre/Documents/kth/Deep_Learning_in_data_science_DD2424/text-generation-project/Run/main_train_lstm.py --hidden-dim 100 --num-layers 1 --word2vec True --mode word --embedding-dim 100 --batch-size 100 --dataset goblet_book.txt
+
+
+#  INFERENCE
+
+/bin/python3.10 /home/pierre/Documents/kth/Deep_Learning_in_data_science_DD2424/text-generation-project/Run/inference_lstm.py --epochs 200 --batch-size 64 --hidden-dim 1024 --num-layer 2 --nucleous True --top-p 0.75
